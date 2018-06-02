01a3ba793eaa26a14c4f95494ccab307518dc443
rhslee2000 <rhslee2000@yahoo.com.au>
2018-06-02 13:43:00 +1000

    added hello world element

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularElements
+# HelloWorldElements
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.3.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { createCustomElement } from '@angular/elements';
+
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [
+	  HelloWorldComponent
+  ],
+  providers: []
 })
-export class AppModule { }
+export class AppModule {
+
+	constructor(private injector: Injector) {
+
+	}
+
+	ngDoBootstrap() {
+		const el = createCustomElement(HelloWorldComponent, {
+			injector: this.injector
+		});
+		customElements.define('hello-world', el);
+	}
+ }
