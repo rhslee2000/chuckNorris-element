f5d29aac5c919ad63f1653cf9a7f2e11b87119e4
rhslee2000 <rhslee2000@yahoo.com.au>
2018-06-23 15:52:14 +1000

    fixed and add tests

--- src/app/chuck-norris/chuck-norris.component.spec.ts
+++ src/app/chuck-norris/chuck-norris.component.spec.ts
@@ -1,14 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { ChuckNorrisComponent } from './chuck-norris.component';
+import { ChuckNorrisService } from './chuck-norris.service';
+import { Joke } from './joke';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/of';
 
 describe('ChuckNorrisComponent', () => {
   let component: ChuckNorrisComponent;
   let fixture: ComponentFixture<ChuckNorrisComponent>;
 
+  const jokeValue = 'test joke';
+  // mock service
+  const joke: Joke = {
+    icon_url: '',
+    value: jokeValue,
+    id: '',
+    url: ''
+  };
+  const mockChuckNorrisService: ChuckNorrisService = jasmine.createSpyObj('ChuckNorrisService', ['getJoke']);
+  (<jasmine.Spy>mockChuckNorrisService.getJoke).and.returnValue(Observable.of(joke));
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChuckNorrisComponent ]
+      declarations: [ChuckNorrisComponent],
+      providers: [
+      { provide: ChuckNorrisService, useValue: mockChuckNorrisService } ]
     })
     .compileComponents();
   }));
@@ -16,10 +36,19 @@ describe('ChuckNorrisComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ChuckNorrisComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load joke', () => {
+    fixture.detectChanges();
+    const mockService = TestBed.get(ChuckNorrisService);
+    expect(mockService.getJoke).toHaveBeenCalled();
+    // test render template
+    const de: DebugElement = fixture.debugElement;
+    expect(de.query(By.css('p')).nativeElement.textContent).toBe(jokeValue);
+
   });
 });
--- src/app/chuck-norris/chuck-norris.service.spec.ts
+++ src/app/chuck-norris/chuck-norris.service.spec.ts
@@ -1,15 +1,39 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ChuckNorrisService } from './chuck-norris.service';
+import { Joke } from './joke';
 
 describe('ChuckNorrisService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ChuckNorrisService]
+		imports: [HttpClientTestingModule],
+      	providers: [ChuckNorrisService]
     });
   });
 
   it('should be created', inject([ChuckNorrisService], (service: ChuckNorrisService) => {
     expect(service).toBeTruthy();
   }));
+
+	describe('getJoke', () => {
+		const jokeValue = 'another funny joke';
+		const mockJoke : Joke = {
+			icon_url: '',
+			value: jokeValue,
+			id: '',
+			url: ''
+		}
+		it('should get the joke', inject([HttpTestingController, ChuckNorrisService],
+			(mockHttp: HttpTestingController, service: ChuckNorrisService) => {
+				service.getJoke().subscribe(
+					response => {
+						expect(response).toBe(mockJoke);
+					});
+				const req = mockHttp.expectOne('https://api.chucknorris.io/jokes/random');
+				expect(req.request.method).toBe('GET');
+				req.flush(mockJoke);
+				mockHttp.verify();
+			}));
+	}); // getJoke()
 });
